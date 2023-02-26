39240835c8429adbd73eea339f8da5e3f1242abd
CenkBerry <cenkberi@gmail.com>
2023-02-26 21:22:06 +0300

    Lesson 2 added

--- react_v18_lessons/src/App.js
+++ react_v18_lessons/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Lesson1 from './Lesson 1 - Understand JSX/Lesson1';
+import Lesson2 from './Lesson 2 - Entering React/Lesson2';
 
 function App() {
   return (
     <>
     <Lesson1 />
+    <Lesson2 />
     </>
   );
 }
---
+++ react_v18_lessons/src/Lesson 2 - React Props and Class/Lesson2.js
@@ -0,0 +1,14 @@
+import { Example1 , Example2 , Example3 , Example4 } from './L2WorkFile';
+
+function Lesson2() {
+    return (
+      <div className="Lesson2">
+        <Example1 />
+        <Example2 />
+        <Example3 />
+        <Example4 />
+      </div>
+    );
+}
+
+export default Lesson2;
